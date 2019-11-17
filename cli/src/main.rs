f5abab852b8c72108d17a3d8754194c0f3b42a76
tommady <tommady@users.noreply.github.com>
2019-11-17 21:02:01 +0800

    add basic config

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,20 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "bros-cli")]
+enum Opt {
+    #[structopt(name = "mybro")]
+    MyBro {
+        #[structopt(parse(from_str))]
+        name: String,
+        #[structopt(long = "p10k-path", default_value = "~/.p10k.zsh")]
+        p10k_path: String,
+        #[structopt(long = "config-path", default_value = "~/.bros.toml")]
+        config_path: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Opt::from_args();
+    println!("{:?}", matches);
 }
