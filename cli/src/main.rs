385d675e2e5efec5ea3b36a99b9414bd11b5eb90
tommady <tommady@users.noreply.github.com>
2019-11-22 20:24:31 +0800

    success write correct command into zshrc

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,9 +1,9 @@
 //use std::error::Error;
-use std::path::Path;
+use dirs::home_dir;
 use std::fs::OpenOptions;
 use std::io::Write;
+use std::path::Path;
 use structopt::StructOpt;
-use dirs::home_dir;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "bros-cli")]
@@ -12,37 +12,31 @@ enum Opt {
     MyBro {
         #[structopt(parse(from_str))]
         name: String,
-        #[structopt(long = "p10k-path")]
-        p10k_path: Option<String>,
+        #[structopt(long = "zshrc-path")]
+        zshrc_path: Option<String>,
     },
 }
 
-fn my_bro(name: String, p10k_path: Option<String>) {
+fn my_bro(name: String, zshrc_path: Option<String>) {
     println!("{}", name);
     let dir = String::from(home_dir().unwrap().to_str().unwrap());
-    let p10k_home = format!("{}/.p10k.zsh", dir);
-    let p10k = match p10k_path {
+    let default_zshrc_path = format!("{}/.zshrc", dir);
+    let zshrc = match zshrc_path {
         Some(ref path) => Path::new(path),
-        None => Path::new(&p10k_home),
+        None => Path::new(&default_zshrc_path),
     };
 
-    let mut file = OpenOptions::new().append(true).open(p10k).unwrap();
-    // let mut w = BufWriter::new(file);
-    let data = "typeset -g POWERLEVEL9K_DIR_CLASSES=('*' DEFAULT ' ❤'${{BRO}}'❤ ')";
-    file.write_all(data.as_bytes()).expect("cannot write file")
-    // w.write_all(data.as_bytes()).expect("unable to write");
-    // w.flush().expect("unable to flush");
-
-    // if let Err(e) = writeln!(file, "typeset -g POWERLEVEL9K_DIR_CLASSES=('*' DEFAULT ' ❤'${{BRO}}'❤ ')") {
-    //     eprintln!("cant write to file: {}", e);
-    // }
+    let mut file = OpenOptions::new().append(true).open(zshrc).unwrap();
+    let mut data = format!("export BRO={}\n", name);
+    file.write(data.as_bytes())
+        .expect("write file export failed");
+    data = "typeset -g POWERLEVEL9K_DIR_CLASSES=('*' DEFAULT ' ❤ '${BRO}' ❤ ')\n".to_string();
+    file.write(data.as_bytes())
+        .expect("write file POWERLEVEL failed");
 }
 
 fn main() {
     match Opt::from_args() {
-        Opt::MyBro {
-            name,
-            p10k_path,
-        } =>  my_bro(name, p10k_path),
+        Opt::MyBro { name, zshrc_path } => my_bro(name, zshrc_path),
     }
 }
