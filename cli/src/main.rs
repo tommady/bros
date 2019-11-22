e1f2ea9bf622b11cac3e6e1b66c34a27bf0b1251
tommady <tommady@users.noreply.github.com>
2019-11-22 17:51:49 +0800

    success write file

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,8 +1,9 @@
-use std::env::var;
-use std::error::Error;
+//use std::error::Error;
 use std::path::Path;
 use std::fs::OpenOptions;
+use std::io::Write;
 use structopt::StructOpt;
+use dirs::home_dir;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "bros-cli")]
@@ -17,16 +18,24 @@ enum Opt {
 }
 
 fn my_bro(name: String, p10k_path: Option<String>) {
-    let p10k_home = format!("{}/.p10k.zsh", var("HOME").unwrap());
+    println!("{}", name);
+    let dir = String::from(home_dir().unwrap().to_str().unwrap());
+    let p10k_home = format!("{}/.p10k.zsh", dir);
     let p10k = match p10k_path {
-        Some(path) => Path::new(&path),
+        Some(ref path) => Path::new(path),
         None => Path::new(&p10k_home),
-    }
-
-    let file = match OpenOptions::new().append(true).open(p10k) {
-        Err(e) => println!("open file {} failed:{}", p10k.display(), e),
-        Ok(_v) => {},
     };
+
+    let mut file = OpenOptions::new().append(true).open(p10k).unwrap();
+    // let mut w = BufWriter::new(file);
+    let data = "typeset -g POWERLEVEL9K_DIR_CLASSES=('*' DEFAULT ' ❤'${{BRO}}'❤ ')";
+    file.write_all(data.as_bytes()).expect("cannot write file")
+    // w.write_all(data.as_bytes()).expect("unable to write");
+    // w.flush().expect("unable to flush");
+
+    // if let Err(e) = writeln!(file, "typeset -g POWERLEVEL9K_DIR_CLASSES=('*' DEFAULT ' ❤'${{BRO}}'❤ ')") {
+    //     eprintln!("cant write to file: {}", e);
+    // }
 }
 
 fn main() {
